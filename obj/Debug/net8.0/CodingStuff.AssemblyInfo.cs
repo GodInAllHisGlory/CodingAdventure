ab3135ef194d6119e9dff6d2497e0324f3ee38b9
Nathan Haight <nathanjohnhaight@gmail.com>
2025-08-12 02:58:33 -0600

    Keep it up to code (yeah I do puns)

--- obj/Debug/net8.0/CodingStuff.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodingStuff.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodingStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bba5a40c445067763477d313cf525d1c32a4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodingStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodingStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
